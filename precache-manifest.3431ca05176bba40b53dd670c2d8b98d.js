57f43487bbaacdbbbe03c91175a7335f7e0e66dc
EstebanV5886 <EstebanV5886@users.noreply.github.com>
2021-12-23 03:26:02 +0000

    deploy: 13dd29740ab72cdc9e73c554e1eccb40f57afdd7

---
+++ precache-manifest.3431ca05176bba40b53dd670c2d8b98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b0890f8b2c093afbf7cc913884f361",
+    "url": "https://EstebanV5886.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "91c2f40d0754e4a187b2",
+    "url": "https://EstebanV5886.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "b69832c72f4f8bd90d72",
+    "url": "https://EstebanV5886.github.io/task-management-frontend/static/js/2.8f9751f0.chunk.js"
+  },
+  {
+    "revision": "91c2f40d0754e4a187b2",
+    "url": "https://EstebanV5886.github.io/task-management-frontend/static/js/main.460ca2cb.chunk.js"
+  },
+  {
+    "revision": "c16ce88dba09941a3200",
+    "url": "https://EstebanV5886.github.io/task-management-frontend/static/js/runtime~main.4dc33d2d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://EstebanV5886.github.io/task-management-frontend/precache-manifest.3431ca05176bba40b53dd670c2d8b98d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://EstebanV5886.github.io/task-management-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
